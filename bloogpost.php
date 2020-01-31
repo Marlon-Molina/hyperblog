8b8168aa3c048e5aadcb6aeab8f364578067d727
Marlon-Molina <marlonr.molina@hotmail.com>
2020-01-31 10:37:00 -0500

    Archivo pasado a PHP dividido header y footer para trabajo de Anita

---
+++ bloogpost.php
@@ -0,0 +1,61 @@
+<?php include "header.php" ?>
+		<div class="container grupo">
+			<div class="caja conatainer">
+				<div class="row">
+					<div class="col-md-1 imagenes">
+						<img class="" src="imagenes/dragon.png" width="40" height="40">
+					</div>
+					<div class="col-md-11">
+						<h3 class="">
+							<strong>
+								Hyperblog 								
+							</strong>
+							<span>Tu blog de cabecera</span>
+						</h3>	
+
+					</div>
+				</div>				
+			</div>
+			<h1 class="text-center">Titulo atractivo e interesante</h1>
+			<div class="row">
+				<p>Lrem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<h3>				
+				<a href="#">Suscribete y dale like</a>
+			</h3>			
+		</div>
+
+		<!-- inicio de formulario -->
+		<div class="container">
+			<form class="caja">
+			  <div class="form-group">
+			    <label for="formGroupExampleInput">Example label</label>
+			    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+			  </div>
+			  <div class="form-group">
+			    <label for="formGroupExampleInput2">Another label</label>
+			    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+			  </div>
+			</form>
+		</div>	
+
+		<!-- fin de formulario -->
+
+<?php include "footer.php" ?>
---
+++ footer.php
@@ -0,0 +1,16 @@
+		<div class="container grupo">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
